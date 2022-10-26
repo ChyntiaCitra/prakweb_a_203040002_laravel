6ea9de5736e2fb88a490e39089f0d59b03cc61c6
ChyntiaCitra <Chyntiacitra04@gmail.com>
2022-10-26 21:33:23 +0700

    menyelesaikan video 9

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
 @section('container')
 <h2>{{ $post->title }}</h2>
 
-<p>By. Chyntia Citra in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</p></a>
+<p>By. <a class="text-decoration-none" href="#">{{ $post->user->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
 
 {!! $post->body !!}}
 
-<a href="/blog">Back</a>
+<a href="/blog" class="d-block mt-3">Back to posts</a>
 @endsection
