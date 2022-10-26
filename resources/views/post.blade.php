63b73d2f0c2f3f6a6e72fd53c59526bcdcbced39
ChyntiaCitra <Chyntiacitra04@gmail.com>
2022-10-26 18:54:23 +0700

    menyelesaikan video 8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,8 @@
 @section('container')
 <h2>{{ $post->title }}</h2>
 
+<p>By. Chyntia Citra in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</p></a>
+
 {!! $post->body !!}}
 
 <a href="/blog">Back</a>
