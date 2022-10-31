e4b4a98ea961994965ea709b19c8d0d04dafae03
ChyntiaCitra <Chyntiacitra04@gmail.com>
2022-10-31 15:37:15 +0700

    menyelesaikan video 12

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,8 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('Home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 
@@ -42,6 +43,7 @@ Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -50,6 +52,7 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post By Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'author'),
     ]);
 });
@@ -57,6 +60,7 @@ Route::get('/categories/{category:slug}', function (Category $category) {
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
         'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author'),
+        'active' => 'categories',
+        'posts' => $author->posts->load('category', 'author')
     ]);
 });
