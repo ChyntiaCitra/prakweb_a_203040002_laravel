1d85f42813682a2479af62b49cb90f0a8fd7548b
ChyntiaCitra <Chyntiacitra04@gmail.com>
2022-11-04 19:38:59 +0700

    menyelesaikan video 14

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,12 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
+  <!-- Boostrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+  <!-- My Style -->
+  <link rel="stylesheet" href="/css/style.css">
+
   <title>Praktikum Web | {{ $title }}</title>
 </head>
 
